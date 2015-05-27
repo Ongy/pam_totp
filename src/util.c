6f680bf0ed30b2e2bc57832152043e2f23e001b8
Markus Ongyerth <ongy@ongy.net>
2015-05-27 18:03:29 +0200

    Fix the read_base32 in util.c

--- src/util.c
+++ src/util.c
@@ -19,6 +19,9 @@
  */
 
 #include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <endian.h>
 #include "util.h"
 
 #define TIME_STEP 30
@@ -26,21 +29,46 @@
 int read_base32(const char *src, uint8_t * dst, size_t maxlen)
 {
 	const char * c;
-	unsigned i;
+	char t;
+	unsigned i, j;
+	uint64_t tmp;
+	uint8_t * tp;
 
-	for(c = src; *c && i < maxlen; ++c, ++i) {
+	j = 0;
+	tmp = 0;
+	tp = (uint8_t *) &tmp;
+	tp += 3;
+
+	for(i = 0, c = src; *c && i * 5 < maxlen; ++c) {
 		if(*c >= 'A' && *c <= 'Z')
-			*(dst+i) = *c - 'A';
+			t = *c - 'A';
 		else if (*c >= '2' && *c <= '7')
-			*(dst+i) = *c - '2' + 26;
+			t = *c - '2' + 26;
 		else if(*c >= 'a' && *c <= 'z')
-			*(dst+i) = *c - 'a';
+			t = *c - 'a';
 		else
 			return -1;
+
+		tmp |= ((uint64_t) t) << (40 - (++j * 5));
+
+		if(j == 8) {
+			j = 5;
+			if(i * 5 + 5 > maxlen)
+				return -1;
+			tmp = htobe64(tmp);
+			memcpy(dst+i*5, tp, 5);
+			tmp = 0;
+			++i;
+			j = 0;
+		}
 	}
 	if(*c)
 		return -1;
-	return i;
+	if(i * 5 + j > maxlen)
+		return -1;
+	tmp = htobe64(tmp);
+	memcpy(dst + i * 5, tp, j);
+	return i * 5 + j;
 }
 
 unsigned get_time_slice()
